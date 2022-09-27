fcee08067de909fc71a81bee9f56f40da03df843
Fatima <108102226+Fatima-ma87@users.noreply.github.com>
2022-09-27 06:33:37 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DummyComponent } from './dummy/dummy.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    NavbarComponent,
+    DummyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
